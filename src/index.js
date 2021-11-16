705fe2f4cf8cffb4fbf2965a2cf8044dfb73d60a
Dmitriy Dudnik <wed2288@gmail.com>
2021-11-16 22:10:36 +0200

    Выполняет 10 дз

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './api-services/api-service';
 import countriesMarkupTpl from './templates/countriesMarkup';
 import coutryMarkupTpl from './templates/coutryMarkup.hbs';
 
@@ -9,18 +11,49 @@ const searchbox = document.querySelector('#search-box');
 const countriesEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
-searchbox.addEventListener('input', e => {
-  e.preventDefault();
-  const name = searchbox.value;
-  fetchCountries(name).then(showCountries);
-});
+let markup = '';
+
+searchbox.addEventListener(
+  'input',
+  debounce(() => {
+    const name = searchbox.value.trim();
+
+    if (name === '') {
+      countriesEl.innerHTML = '';
+      countryInfoEl.innerHTML = '';
+      return;
+    }
+    fetchCountries(name).then(showCountries).catch(showError);
+  }, DEBOUNCE_DELAY),
+);
 
 function showCountries(name) {
-  const coutryOneMarkup = coutryMarkupTpl(name);
-  countryInfoEl.innerHTML = coutryOneMarkup;
-  console.log(coutryOneMarkup);
+  countriesEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+
+  if (name.length > 10) {
+    return Notify.success('Too many matches found. Please enter a more specific name.');
+  }
+
+  if (name.length >= 2 && name.length <= 10) {
+    markup = countriesMarkup(name);
+    return countriesEl.insertAdjacentHTML('beforeend', markup);
+  }
+
+  markup = oneCountryMarkup(name);
+  return countryInfoEl.insertAdjacentHTML('beforeend', markup);
 }
 
-function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(res => res.json());
+function countriesMarkup(name) {
+  return countriesMarkupTpl(name);
+}
+
+function oneCountryMarkup(name) {
+  return coutryMarkupTpl(name);
+}
+
+function showError() {
+  countriesEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+  return Notify.failure('Oops, there is no country with that name');
 }
